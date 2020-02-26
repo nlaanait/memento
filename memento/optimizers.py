4b674bf14bf68ae4ce50c10431337fb6e2181619
Massimiliano Lupo Pasini <7ml@ornl.gov>
2020-02-26 11:00:37 -0500

    removed dunders from variable in classes

--- memento/optimizers.py
+++ memento/optimizers.py
@@ -46,11 +46,6 @@ class Richardson(object):
         self.iteration_count = 0
         self.verbose = verbose
 
-    @classmethod
-    def build(cls):
-        obj  = cls(*args)
-        return obj
-
     def print_verbose(self, *args, **kwargs):
         if self.verbose :
             print(*args, **kwargs)
@@ -58,26 +53,27 @@ class Richardson(object):
     def solve(self, initial_guess):
         ## TODO: consider passing initial guess to solve()
 
-        __residual = self.rhs - self.matrix @ initial_guess
-        __residual_norm = __residual.norm()
-        __relative_residual_norm = __residual_norm / self.rhs_norm
+        residual = self.rhs - self.matrix @ initial_guess
+        residual_norm = residual.norm()
+        relative_residual_norm = residual_norm / self.rhs_norm
 
-        __solution = initial_guess
+        solution = initial_guess
 
-        while __relative_residual_norm > self.tol and self.iteration_count < self.maxiter:
+        while relative_residual_norm > self.tol and self.iteration_count < self.maxiter:
             ## TODO: consider making all of these non-attributes and just return them
-            __solution = __solution + self.relaxation * __residual
+            solution = solution + self.relaxation * residual
             
-            __residual = self.rhs - torch.matmul(self.matrix, __solution)
-            __residual_norm = __residual.norm()
-            relative_residual_norm = __residual_norm / self.rhs_norm
+            residual = self.rhs - torch.matmul(self.matrix, solution)
+            residual_norm = residual.norm()
+            relative_residual_norm = residual_norm / self.rhs_norm
             self.iteration_count += 1
             self.print_verbose("Richardson converged in ", str(self.iteration_count), " iteration with relative residual norm: ",
-                                     str(__relative_residual_norm), end='...')
+                                     str(relative_residual_norm), end='...')
 
         # Do not return because it's already an attribute
-        return __solution
+        return solution
 
+"""
 class Optimizer:
 
     def __init__(self, iteration, tolerance, device="cpu"):
@@ -87,15 +83,6 @@ class Optimizer:
 
     def solve(self):
         raise NotImplementedError
-
-    @classmethod
-    def build(cls):
-        obj  = cls(*args)
-        return obj
-
-    @staticmethod
-    def delete(something):
-        del something
 
 
 class SpecialOptimizer(Optimizer):
@@ -108,11 +95,9 @@ class SpecialOptimizer(Optimizer):
 
     def solve(self):
         pass
+"""
 
 class ConjugateGradient(object):
-
-    #CAVEAT: functions f and g are used as external objects
-    #CAVEAT: x, y
 
     def __init__(self, nsteps=10, residual_tol=1e-18, lr=1.0, verbose=True):
 
@@ -128,28 +113,28 @@ class ConjugateGradient(object):
 
     def solve(self, f, g, x, y):
 
-        __f_history = []
-        __g_history = []
-        __x_history = []
-        __y_history = []
+        f_history = []
+        g_history = []
+        x_history = []
+        y_history = []
 
-        __f_history.append(f(x, y))
-        __g_history.append(g(x, y))
-        __x_history.append(x)
-        __y_history.append(y)
+        f_history.append(f(x, y))
+        g_history.append(g(x, y))
+        x_history.append(x)
+        y_history.append(y)
 
         while self.iter_count < self.nsteps:
             self.iter_count += 1
 
-            __f_eval = f(x, y)
-            __g_eval = g(x, y)
-            __grad_f_x = autograd.grad(__f_eval, x, create_graph=True, allow_unused=True)
-            __grad_g_y = autograd.grad(__g_eval, y, create_graph=True, allow_unused=True)
+            f_eval = f(x, y)
+            g_eval = g(x, y)
+            grad_f_x = autograd.grad(f_eval, x, create_graph=True, allow_unused=True)
+            grad_g_y = autograd.grad(g_eval, y, create_graph=True, allow_unused=True)
 
-            __new_x = x - self.lr * __grad_f_x[0]
-            __new_y = y - self.lr * __grad_g_y[0]
-            x = __new_x.clone().detach().requires_grad_(True)
-            y = __new_y.clone().detach().requires_grad_(True)
+            new_x = x - self.lr * grad_f_x[0]
+            new_y = y - self.lr * grad_g_y[0]
+            x = new_x.clone().detach().requires_grad_(True)
+            y = new_y.clone().detach().requires_grad_(True)
 
             self.print_verbose("######################################################")
             self.print_verbose("Iteration: ", self.iter_count)
@@ -159,12 +144,12 @@ class ConjugateGradient(object):
             self.print_verbose("g(x,y): ", g(x, y))
             self.print_verbose("######################################################")
 
-            __f_history.append(f(x, y))
-            __g_history.append(g(x, y))
-            __x_history.append(x)
-            __y_history.append(y)
+            f_history.append(f(x, y))
+            g_history.append(g(x, y))
+            x_history.append(x)
+            y_history.append(y)
 
-        return __f_history, __g_history, __x_history, __y_history
+        return f_history, g_history, x_history, y_history
 
 
 class CompetitiveGradient(object):
@@ -183,50 +168,50 @@ class CompetitiveGradient(object):
 
     def solve(self, f, g, x, y):
 
-        __f_history = []
-        __g_history = []
-        __x_history = []
-        __y_history = []
+        f_history = []
+        g_history = []
+        x_history = []
+        y_history = []
 
-        __x_history.append(x)
-        __y_history.append(y)
+        x_history.append(x)
+        y_history.append(y)
 
         while self.iter_count < self.nsteps:
             self.iter_count += 1
-            __f_val = f(x, y)
-            __g_val = g(x, y)
-            __grad_f_x, = autograd.grad(__f_val, x, create_graph=True, allow_unused=True)
-            __grad_g_y, = autograd.grad(__g_val, y, create_graph=True, allow_unused=True)
+            f_val = f(x, y)
+            g_val = g(x, y)
+            grad_f_x, = autograd.grad(f_val, x, create_graph=True, allow_unused=True)
+            grad_g_y, = autograd.grad(g_val, y, create_graph=True, allow_unused=True)
             if not self.full_hessian:
-                __hess_f_xy = hessian_vec(__grad_f_x, y, retain_graph=False)
-                __hess_g_yx = hessian_vec(__grad_g_y, x, retain_graph=False)
-                __x_rhs = __grad_f_x - self.lr * torch.matmul(__hess_f_xy,  __grad_g_y)
-                __y_rhs = __grad_g_y - self.lr * torch.matmul(__hess_g_yx, __grad_f_x)
+                hess_f_xy = hessian_vec(grad_f_x, y, retain_graph=False)
+                hess_g_yx = hessian_vec(grad_g_y, x, retain_graph=False)
+                x_rhs = grad_f_x - self.lr * torch.matmul(hess_f_xy,  grad_g_y)
+                y_rhs = grad_g_y - self.lr * torch.matmul(hess_g_yx, grad_f_x)
                 # The "*" multiplication operates elementwise
                 # We have to use the "*" and not the matmul method because we do NOT extract the entire Hessian matrix, we just
                 # extract the diagonal entries
                 #__x_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.diag_embed(torch.matmul(__hess_f_xy, __hess_g_yx))
-                __x_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.diag_embed(__hess_f_xy * __hess_g_yx)
+                x_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.diag_embed(hess_f_xy * hess_g_yx)
                 #__y_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.diag_embed(torch.matmul(__hess_g_yx, __hess_f_xy))
-                __y_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.diag_embed(__hess_g_yx * __hess_f_xy)
+                y_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.diag_embed(hess_g_yx * hess_f_xy)
             else:
-                __hess_f_xy = hessian(__grad_f_x, y, retain_graph=False)
-                __hess_g_yx = hessian(__grad_g_y, x, retain_graph=False)
-                __x_rhs = __grad_f_x - self.lr * torch.matmul(__hess_f_xy, __grad_g_y)
-                __y_rhs = __grad_g_y - self.lr * torch.matmul(__hess_g_yx, __grad_f_x)
-                __x_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.matmul(__hess_f_xy, __hess_g_yx)
-                __y_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.matmul(__hess_g_yx, __hess_f_xy)
+                hess_f_xy = hessian(grad_f_x, y, retain_graph=False)
+                hess_g_yx = hessian(grad_g_y, x, retain_graph=False)
+                x_rhs = grad_f_x - self.lr * torch.matmul(hess_f_xy, grad_g_y)
+                y_rhs = grad_g_y - self.lr * torch.matmul(hess_g_yx, grad_f_x)
+                x_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.matmul(hess_f_xy, hess_g_yx)
+                y_A = torch.eye(x.shape[0]) - self.lr * self.lr * torch.matmul(hess_g_yx, hess_f_xy)
 
-            __solver1 = Richardson(__x_A, __x_rhs, 1e-10, 1000, 1, verbose=False)
-            __initial_guess_x = torch.randn(__x_rhs.shape)
-            __delta_x = __solver1.solve(__initial_guess_x)
-            __solver2 = Richardson(__y_A, __y_rhs, 1e-10, 1000, 1, verbose=False)
-            __initial_guess_y = torch.randn(__y_rhs.shape)
-            __delta_y = __solver2.solve(__initial_guess_y)
-            __new_x = x - self.lr * __delta_x
-            __new_y = y - self.lr * __delta_y
-            x = __new_x.clone().detach().requires_grad_(True)
-            y = __new_y.clone().detach().requires_grad_(True)
+            solver1 = Richardson(x_A, x_rhs, 1e-10, 1000, 1, verbose=False)
+            initial_guess_x = torch.randn(x_rhs.shape)
+            delta_x = solver1.solve(initial_guess_x)
+            solver2 = Richardson(y_A, y_rhs, 1e-10, 1000, 1, verbose=False)
+            initial_guess_y = torch.randn(y_rhs.shape)
+            delta_y = solver2.solve(initial_guess_y)
+            new_x = x - self.lr * delta_x
+            new_y = y - self.lr * delta_y
+            x = new_x.clone().detach().requires_grad_(True)
+            y = new_y.clone().detach().requires_grad_(True)
 
             self.print_verbose("######################################################")
             self.print_verbose("Iteration: ", self.iter_count)
@@ -234,16 +219,16 @@ class CompetitiveGradient(object):
             self.print_verbose("y: ", y)
             self.print_verbose("f(x,y): ", f(x, y))
             self.print_verbose("g(x,y): ", g(x, y))
-            self.print_verbose("hess_f_xy:", __hess_f_xy)
-            self.print_verbose("hess_g_yx:", __hess_g_yx)
+            self.print_verbose("hess_f_xy:", hess_f_xy)
+            self.print_verbose("hess_g_yx:", hess_g_yx)
             self.print_verbose("######################################################")
 
-            __f_history.append(f(x, y))
-            __g_history.append(g(x, y))
-            __x_history.append(x)
-            __y_history.append(y)
+            f_history.append(f(x, y))
+            g_history.append(g(x, y))
+            x_history.append(x)
+            y_history.append(y)
 
-        return __f_history, __g_history, __x_history, __y_history
+        return f_history, g_history, x_history, y_history
 
 
 class CompetitiveGradientJacobi(object):
@@ -262,57 +247,57 @@ class CompetitiveGradientJacobi(object):
 
     def solve(self, f, g, x, y, delay=1):
 
-        __x_buffer = []
-        __y_buffer = []
-        __f_history = []
-        __g_history = []
-        __x_history = []
-        __y_history = []
+        x_buffer = []
+        y_buffer = []
+        f_history = []
+        g_history = []
+        x_history = []
+        y_history = []
 
-        __prev_y = y.clone().detach().requires_grad_(True)
-        __prev_x = x.clone().detach().requires_grad_(True)
-        __x_history.append(x)
-        __y_history.append(y)
+        prev_y = y.clone().detach().requires_grad_(True)
+        prev_x = x.clone().detach().requires_grad_(True)
+        x_history.append(x)
+        y_history.append(y)
 
         while self.iter_count < self.nsteps:
 
             self.iter_count += 1
-            __f_val_x = f(x, __prev_y)
-            __f_val_y = f(__prev_x, y)
-            __g_val_x = g(x, __prev_y)
-            __g_val_y = g(__prev_x, y)
-            __grad_f_x_x, = autograd.grad(__f_val_x, x, create_graph=True,
+            f_val_x = f(x, prev_y)
+            f_val_y = f(prev_x, y)
+            g_val_x = g(x, prev_y)
+            g_val_y = g(prev_x, y)
+            grad_f_x_x, = autograd.grad(f_val_x, x, create_graph=True,
                                         allow_unused=True)  # terrible variable name, implies diagonal hessian!!
-            __grad_f_x_y, = autograd.grad(__f_val_y, __prev_x, create_graph=True,
+            grad_f_x_y, = autograd.grad(f_val_y, prev_x, create_graph=True,
                                         allow_unused=True)  # terrible variable name, implies diagonal hessian!!
-            __grad_g_y_x, = autograd.grad(__g_val_x, __prev_y, create_graph=True, allow_unused=True)
-            __grad_g_y_y, = autograd.grad(__g_val_y, y, create_graph=True, allow_unused=True)
+            grad_g_y_x, = autograd.grad(g_val_x, prev_y, create_graph=True, allow_unused=True)
+            grad_g_y_y, = autograd.grad(g_val_y, y, create_graph=True, allow_unused=True)
 
             if not self.full_hessian:
 
-                __hess_f_xy_x = hessian_vec(__grad_f_x_x, __prev_y, retain_graph=False)
-                __hess_f_xy_y = hessian_vec(__grad_f_x_y, y, retain_graph=False)
-                __hess_g_yx_x = hessian_vec(__grad_g_y_x, x, retain_graph=False)
-                __hess_g_yx_y = hessian_vec(__grad_g_y_y, __prev_x, retain_graph=False)
-                __delta_x = -self.lr * (__grad_f_x_x + 2 * __hess_f_xy_x * __grad_g_y_x)
-                __delta_y = -self.lr * (__grad_g_y_y + 2 * __hess_g_yx_y * __grad_f_x_y)
+                hess_f_xy_x = hessian_vec(grad_f_x_x, prev_y, retain_graph=False)
+                hess_f_xy_y = hessian_vec(grad_f_x_y, y, retain_graph=False)
+                hess_g_yx_x = hessian_vec(grad_g_y_x, x, retain_graph=False)
+                hess_g_yx_y = hessian_vec(grad_g_y_y, prev_x, retain_graph=False)
+                delta_x = -self.lr * (grad_f_x_x + 2 * hess_f_xy_x * grad_g_y_x)
+                delta_y = -self.lr * (grad_g_y_y + 2 * hess_g_yx_y * grad_f_x_y)
             else:
 
-                __hess_f_xy_x = hessian(__grad_f_x_x, __prev_y, retain_graph=False)
-                __hess_f_xy_y = hessian(__grad_f_x_y, y, retain_graph=False)
-                __hess_g_yx_x = hessian(__grad_g_y_x, x, retain_graph=False)
-                __hess_g_yx_y = hessian(__grad_g_y_y, __prev_x, retain_graph=False)
-                __delta_x = -self.lr * (__grad_f_x_x + 2 * torch.matmul(__hess_f_xy_x, __grad_g_y_x))
-                __delta_y = -self.lr * (__grad_g_y_y + 2 * torch.matmul(__hess_g_yx_y, __grad_f_x_y))
+                hess_f_xy_x = hessian(grad_f_x_x, prev_y, retain_graph=False)
+                hess_f_xy_y = hessian(grad_f_x_y, y, retain_graph=False)
+                hess_g_yx_x = hessian(grad_g_y_x, x, retain_graph=False)
+                hess_g_yx_y = hessian(grad_g_y_y, prev_x, retain_graph=False)
+                delta_x = -self.lr * (grad_f_x_x + 2 * torch.matmul(hess_f_xy_x, grad_g_y_x))
+                delta_y = -self.lr * (grad_g_y_y + 2 * torch.matmul(hess_g_yx_y, grad_f_x_y))
 
-            __new_x = x - self.lr * __delta_x
-            __new_y = y - self.lr * __delta_y
+            new_x = x - self.lr * delta_x
+            new_y = y - self.lr * delta_y
 
-            x = __new_x.clone().detach().requires_grad_(True)
-            y = __new_y.clone().detach().requires_grad_(True)
+            x = new_x.clone().detach().requires_grad_(True)
+            y = new_y.clone().detach().requires_grad_(True)
 
-            __x_buffer.append(x)
-            __y_buffer.append(y)
+            x_buffer.append(x)
+            y_buffer.append(y)
 
             self.print_verbose("######################################################")
             self.print_verbose("Iteration: ", self.iter_count)
@@ -320,22 +305,22 @@ class CompetitiveGradientJacobi(object):
             self.print_verbose("y: ", y)
             self.print_verbose("f(x,y): ", f(x, y))
             self.print_verbose("g(x,y): ", g(x, y))
-            self.print_verbose("hess_f_xy_x:", __hess_f_xy_x)
-            self.print_verbose("hess_f_xy_y:", __hess_f_xy_y)
-            self.print_verbose("hess_g_yx_x:", __hess_g_yx_x)
-            self.print_verbose("hess_g_yx_y:", __hess_g_yx_y)
+            self.print_verbose("hess_f_xy_x:", hess_f_xy_x)
+            self.print_verbose("hess_f_xy_y:", hess_f_xy_y)
+            self.print_verbose("hess_g_yx_x:", hess_g_yx_x)
+            self.print_verbose("hess_g_yx_y:", hess_g_yx_y)
             self.print_verbose("######################################################")
 
-            __f_history.append(f(x, y))
-            __g_history.append(g(x, y))
-            __x_history.append(x)
-            __y_history.append(y)
+            f_history.append(f(x, y))
+            g_history.append(g(x, y))
+            x_history.append(x)
+            y_history.append(y)
 
             if self.iter_count > delay:
-                __prev_y = __y_buffer[self.iter_count - delay].clone().detach().requires_grad_(True)
-                __prev_x = __x_buffer[self.iter_count - delay].clone().detach().requires_grad_(True)
+                prev_y = y_buffer[self.iter_count - delay].clone().detach().requires_grad_(True)
+                prev_x = x_buffer[self.iter_count - delay].clone().detach().requires_grad_(True)
 
-        return __f_history, __g_history, __x_history, __y_history
+        return f_history, g_history, x_history, y_history
 
 
 
